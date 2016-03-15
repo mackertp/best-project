52af9da7e7ebafdf7693ec6e83bf9490e296ae8e
Robert Bofinger <bofinger@canisius.edu>
2016-03-15 16:49:40 -0700

    add get and set category methods

--- src/Data.java
+++ src/Data.java
@@ -7,22 +7,22 @@ public class Data {
     
     public Data(File dataFile) throws FileNotFoundException{
 
-        int[][] dataArray = new int[989818][17];
+        msnbcData = new DataArray(989818, 17);
         int lineNum = 0;
 
         Scanner file = new Scanner(dataFile);
 
         while (file.hasNextLine()){
-            String line = file.nextLine();
+            Scanner line = new Scanner(file.nextLine());
             lineNum+=1;
             
-            for (int i=0; i<line.length(); i++){
-                int view = (int)line.charAt(i);
-                dataArray[lineNum][view] += 1;
+            while (line.hasNextInt()){
+                int category = line.nextInt() - 1; // views are stored in file as 1-17 but stored in program as 0-16
+                msnbcData.setCategory(lineNum, category, msnbcData.getCategory(lineNum, category) + 1); // increment category
             }
         }
 
-        msnbcData.setData(dataArray);
+
     }
 }
 
--- src/DataArray.java
+++ src/DataArray.java
@@ -1,13 +1,18 @@
+import java.util.AbstractList;
 
-public class DataArray {
+public class DataArray{
     int[][] theArray;
 
     public DataArray(int rows, int cols){
         theArray = new int[rows][cols];
     }
 
-    public void setData(int[][] dataArray){
-        theArray = dataArray;
+    public void setCategory(int user, int category, int val){
+        theArray[user][category] = val;
+    }
+
+    public int getCategory(int user, int category){
+        return theArray[user][category];
     }
 }
 
