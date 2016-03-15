af01417f2801c554a9bae5799396cb5512af8139
preston <mackertp@my.canisius.edu>
2016-03-15 19:10:35 -0400

    moved the splash screen into the GUI class

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,140 @@
 
-public class GUI {
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.collections.*;
+import javafx.concurrent.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.util.Duration;
 
+/**
+ * Example of displaying a splash page for a standalone JavaFX application
+ */
+
+public class GUI extends Application {
+
+    public static final String APPLICATION_ICON =
+            "http://static.tvtropes.org/pmwiki/pub/images/new-msnbc-logo-781050.gif";
+    public static final String SPLASH_IMAGE =
+            "http://www.msnbc.com/sites/msnbc/themes/leanforward/images/site-header/msnbc-logo-card-twitter.png";
+
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private Stage mainStage;
+    private static final int SPLASH_WIDTH = 600;
+    private static final int SPLASH_HEIGHT = 400;
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        ImageView splash = new ImageView(new Image(
+                SPLASH_IMAGE
+        ));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH+210);
+        progressText = new Label("importing data from msnbc . . .");
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress, progressText);
+        progressText.setAlignment(Pos.CENTER);
+        splashLayout.setStyle("-fx-padding: 5; " + "-fx-background-color: white; " + "-fx-border-width:2; " + "-fx-border-color: " +
+                "linear-gradient(" + "to bottom, " + "blue, " + "derive(blue, 20%)" + ");");
+        splashLayout.setEffect(new DropShadow());
+    }
+
+    @Override
+    public void start(final Stage initStage) throws Exception {
+        final Task<ObservableList<String>> friendTask = new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws InterruptedException {
+                ObservableList<String> foundFriends =
+                        FXCollections.<String>observableArrayList();
+                ObservableList<String> availableFriends =
+                        FXCollections.observableArrayList(
+                                "Fili", "Kili", "Oin", "Gloin", "Thorin",
+                                "Dwalin", "Balin", "Bifur", "Bofur",
+                                "Bombur", "Dori", "Nori", "Ori"
+                        );
+
+                for (int i = 0; i < availableFriends.size(); i++) {
+                    Thread.sleep(400);
+                    updateProgress(i + 1, availableFriends.size());
+                    String nextFriend = availableFriends.get(i);
+                    foundFriends.add(nextFriend);
+                    updateMessage("Loading in user data...");
+                }
+                Thread.sleep(400);
+                updateMessage("Data Retrieved");
+
+                return foundFriends;
+            }
+        };
+
+        showSplash(
+                initStage,
+                friendTask,
+                () -> showMainStage(friendTask.valueProperty())
+        );
+        new Thread(friendTask).start();
+    }
+
+    private void showMainStage(
+            ReadOnlyObjectProperty<ObservableList<String>> friends
+    ) {
+        mainStage = new Stage(StageStyle.DECORATED);
+        mainStage.setTitle("My Friends");
+        mainStage.getIcons().add(new Image(
+                APPLICATION_ICON
+        ));
+
+        final ListView<String> peopleView = new ListView<>();
+        peopleView.itemsProperty().bind(friends);
+
+        mainStage.setScene(new Scene(peopleView));
+        mainStage.show();
+    }
+
+    private void showSplash(
+            final Stage initStage,
+            Task<?> task,
+            InitCompletionHandler initCompletionHandler
+    ) {
+        progressText.textProperty().bind(task.messageProperty());
+        loadProgress.progressProperty().bind(task.progressProperty());
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                loadProgress.progressProperty().unbind();
+                loadProgress.setProgress(1);
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+
+                initCompletionHandler.complete();
+            } // todo add code to gracefully handle other task states.
+        });
+
+        Scene splashScene = new Scene(splashLayout);
+        initStage.initStyle(StageStyle.UNDECORATED);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.show();
+    }
+
+    public interface InitCompletionHandler {
+        public void complete();
+    }
 }
