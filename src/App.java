05cc5179e9028f5d1d6c8747215583b0cc13b9ae
preston <mackertp@my.canisius.edu>
2016-03-15 18:22:49 -0400

    changed the ArrayDict to DataArray and started the Data class and DataArray class

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("jugglerrob is so cool and handsome");
+        System.out.println("");
     }
 }
--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class Data {
 
+    public Data(File dataFile) throws FileNotFoundException{
+        Scanner file = new Scanner(dataFile);
+
+        while (file.hasNextLine()){
+            String line = file.nextLine();
+
+        }
+
+    }
 }
