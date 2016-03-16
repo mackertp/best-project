36311a6ae5fdf70d899613ec979ea5703107d518
Robert Bofinger <bofinger@canisius.edu>
2016-03-15 17:18:22 -0700

    add cmd line progress tracker when loading data

--- src/Data.java
+++ src/Data.java
@@ -4,35 +4,41 @@ import java.util.Scanner;
 public class Data {
 
     private DataArray msnbcData;
-    private int linesProcessed; // represents how many lines are loaded
-    private int totalLines = 989818;
-    private int categories = 17;
+    private int usersProcessed; // represents how many lines are loaded
+    private int totalUsers;
+    private int categories;
     
-    public Data(File dataFile) throws FileNotFoundException{
+    public Data(int totalUsers, int categories) {
+        msnbcData = new DataArray(totalUsers, categories);
+        this.totalUsers = totalUsers;
+        this.categories = categories;
+    }
 
-        msnbcData = new DataArray(totalLines, categories);
+    public void loadData(File dataFile) throws FileNotFoundException{
         int lineNum = 0;
 
         Scanner file = new Scanner(dataFile);
 
         while (file.hasNextLine()){
             Scanner line = new Scanner(file.nextLine());
-            lineNum+=1;
             
             while (line.hasNextInt()){
                 int category = line.nextInt() - 1; // views are stored in file as 1-17 but stored in program as 0-16
                 msnbcData.setCategory(lineNum, category, msnbcData.getCategory(lineNum, category) + 1); // increment category
             }
+
+            usersProcessed++;
+            lineNum++;
         }
 
     }
 
-    public int getLinesProcessed(){
-        return linesProcessed;
+    public int getUsersProcessed(){
+        return usersProcessed;
     }
 
-    public int getTotalLines(){
-        return totalLines;
+    public int getTotalUsers(){
+        return totalUsers;
     }
 }
 
